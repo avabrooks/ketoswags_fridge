5b179a081af412d4b90fa0cff9c806fe576660e6
risaiwazaki <risaiwazaki55@gmail.com>
2021-10-17 23:35:18 -0700

    Ascii art html page

--- src/main/java/com/example/sping_portfolio/asciiproj/asciibeginner.java
+++ src/main/java/com/example/sping_portfolio/asciiproj/asciibeginner.java
@@ -1,16 +1,9 @@
 package com.example.sping_portfolio.asciiproj;
 
 public class asciibeginner {
-    /**
-     *
-     * @param x A 2D array to print
-     * @param showCommas Whether or not to display the commas
-     */
     public static void printArray(double[][] x, boolean showCommas) {
         // Loops through rows
         for (int n = 0; n < x.length; n++) {
-            // For each row, I loop through all of the elements of
-            // that row (all of the columns)
             for (int a = 0; a < x[n].length; a++) {
                 System.out.print(x[n][a]);
                 if (showCommas && a < x[n].length - 1) {
@@ -23,7 +16,6 @@ public class asciibeginner {
             System.out.print("\n");
         }
     }
-
     public static void main(String[] args) {
         double[][] x = new double[20][15];
 
