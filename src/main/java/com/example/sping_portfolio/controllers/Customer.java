1e24db5b414d0dc65a18a3e641fababe572b0752
crystalwidjaja <pansypig@gmail.com>
2021-10-24 19:24:37 -0700

    backlog fix

--- src/main/java/com/example/sping_portfolio/controllers/Customer.java
+++ src/main/java/com/example/sping_portfolio/controllers/Customer.java
@@ -5,9 +5,8 @@ import lombok.Data;
 @Data
 public class Customer {
 
-    private String firstName;
-    private String lastName;
-    private String email;
-    private int age;
+    private String recipeName;
+    private String recipeType;
+    private String dateAccessed;
 
 }
