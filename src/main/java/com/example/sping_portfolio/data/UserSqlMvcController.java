9e43509af5086d5ca459815eed6ab4069d9accb0
avabrooks <72889462+avabrooks@users.noreply.github.com>
2021-10-27 10:32:30 -0700

    errors

---
+++ src/main/java/com/example/sping_portfolio/data/UserSqlMvcController.java
@@ -0,0 +1,30 @@
+package com.example.sping_portfolio.data;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Controller
+public class UserSqlMvcController implements WebMvcConfigurer {
+
+    // Autowired enables Control to connect HTML and POJO Object to Database easily for CRUD
+    @Autowired
+    private UserSqlRepository repository;
+
+    @GetMapping("/data/user")
+    public String user(Model model) {
+        List<User> list = repository.listAll();
+        model.addAttribute("list", list);
+        return "data/user";
+    }
+
+    /*  The HTML template Forms and userForm attributes are bound
+        @return - template for user form
+        @param - user Class
+    */
+
--- src/main/java/com/example/sping_portfolio/user/userJpaRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.sping_portfolio.user;
-
-public interface userJpaRepository {
-}
